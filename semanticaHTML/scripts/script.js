89325ecb1df90102ae19d41c4e771088061db3c0
Guilherme Amorim <cezarguilherme03@gmail.com>
2024-03-18 21:48:59 -0300

    realizando configuração do toggle menu e theme

--- semanticaHTML/scripts/script.js
+++ semanticaHTML/scripts/script.js
@@ -1,5 +1,34 @@
-const headerLinks = document.querySelector('.header-style__links');
+const headerLinks = document.querySelector(".header-style__links");
 
-document.getElementById('handleNavbar').addEventListener('click', () => {
-    headerLinks.classList.toggle('open');
+document.getElementById("handleNavbar").addEventListener('click', () => {
+    headerLinks.classList.toggle("open")
 })
+
+function setTheme(theme) {
+    localStorage.setItem("theme", theme);
+    document.documentElement.className = theme;
+  }
+  
+
+function toggleTheme() {
+    if (localStorage.getItem("theme") === "theme-dark") {
+        document
+            .getElementById("toggle")
+            .classList.replace("bi-brightness-high-fill", "bi-moon-fill");
+
+        setTheme("theme-light");
+    } else {
+        document
+            .getElementById("toggle")
+            .classList.replace("bi-moon-fill", "bi-brightness-high-fill");
+        setTheme("theme-dark");
+    }
+}
+
+(function () {
+    if (localStorage.getItem("theme") === "theme-dark") {
+        setTheme("theme-dark");
+    } else {
+        setTheme("theme-light");
+    }
+})();
