420a057cbe1aab4ad2930315c991e424df331651
guilherme amorim <cezarguilherme03@gmail.com>
2024-03-19 18:27:49 -0300

    realizando toggle de tema

--- semanticaHTML/scripts/script.js
+++ semanticaHTML/scripts/script.js
@@ -1,28 +1,30 @@
+//Função para realizar o toggle do menu hamburger
 const headerLinks = document.querySelector(".header-style__links");
 
-document.getElementById("handleNavbar").addEventListener('click', () => {
-    headerLinks.classList.toggle("open")
-})
+document.getElementById("handleNavbar").addEventListener("click", () => {
+  headerLinks.classList.toggle("open");
+});
 
+//função para setar o tema no local storage do navegador
 function setTheme(theme) {
-    localStorage.setItem("theme", theme);
-    document.documentElement.className = theme;
-  }
-  
+  localStorage.setItem("theme", theme);
+  document.documentElement.className = theme;
+}
 
+//função para capturar o tema no local storage do navegador
 function toggleTheme() {
-    if (localStorage.getItem("theme") === "theme-dark") {
-        document
-            .getElementById("toggle")
-            .classList.replace("bi-brightness-high-fill", "bi-moon-fill");
+  if (localStorage.getItem("theme") === "theme-dark") {
+    document
+      .getElementById("toggle")
+      .classList.replace("bi-brightness-high-fill", "bi-moon-fill");
 
-        setTheme("theme-light");
-    } else {
-        document
-            .getElementById("toggle")
-            .classList.replace("bi-moon-fill", "bi-brightness-high-fill");
-        setTheme("theme-dark");
-    }
+    setTheme("theme-light");
+  } else {
+    document
+      .getElementById("toggle")
+      .classList.replace("bi-moon-fill", "bi-brightness-high-fill");
+    setTheme("theme-dark");
+  }
 }
 
 (function () {
