015dd41e2f30b9a89cab84ff250829243ecb4c94
guilherme amorim <cezarguilherme03@gmail.com>
2024-03-19 22:04:03 -0300

    Quase finalizando

--- semanticaHTML/scripts/script.js
+++ semanticaHTML/scripts/script.js
@@ -1,18 +1,53 @@
-//Função para realizar o toggle do menu hamburger
+// Função para fechar os modais de links
 const headerLinks = document.querySelector(".header-style__links");
+const headerLinks2 = document.querySelector(".header-style__links2");
 
-document.getElementById("handleNavbar").addEventListener("click", () => {
-  headerLinks.classList.toggle("open");
-});
+function fecharTodosModais() {
+  headerLinks.classList.remove("open");
+  headerLinks2.classList.remove("open2");
+}
 
-//função para setar o tema no local storage do navegador
+function toggleModal1() {
+  fecharTodosModais();
+  headerLinks.classList.add("open");
+}
+
+function toggleModal2() {
+  fecharTodosModais();
+  headerLinks2.classList.add("open2");
+}
+
+document.getElementById("handleNavbar").addEventListener("click", toggleModal1);
+
+// Ouvinte de evento para abrir ou fechar o modal 2
+document
+  .getElementById("handleNavbar2")
+  .addEventListener("click", toggleModal2);
+
+// function fecharModais() {
+//     headerLinks.classList.remove("open");
+//     headerLinks2.classList.remove("open2");
+//   }
+
+//   // Ouvinte de evento para fechar o modal 1
+//   document.getElementById("handleNavbar").addEventListener("click", () => {
+//     headerLinks.classList.remove("open");
+//     fecharModais(); // Chamando a função para fechar ambos os modais
+//   });
+
+//   // Ouvinte de evento para fechar o modal 2
+//   document.getElementById("handleNavbar2").addEventListener("click", () => {
+//     headerLinks2.classList.remove("open2");
+//     fecharModais(); // Chamando a função para fechar ambos os modais
+//   });
+
+//Função para setar o tema no local storage do navegador
 function setTheme(theme) {
   localStorage.setItem("theme", theme);
   document.documentElement.className = theme;
 }
 
-//função para capturar o tema no local storage do navegador
-function toggleTheme() {
+function getTheme() {
   if (localStorage.getItem("theme") === "theme-dark") {
     document
       .getElementById("toggle")
@@ -23,14 +58,20 @@ function toggleTheme() {
     document
       .getElementById("toggle")
       .classList.replace("bi-moon-fill", "bi-brightness-high-fill");
+
     setTheme("theme-dark");
   }
 }
 
-(function () {
-    if (localStorage.getItem("theme") === "theme-dark") {
-        setTheme("theme-dark");
-    } else {
-        setTheme("theme-light");
-    }
+//função imediatamente invocada - IIFE (Immediately Invoked Function Expression)
+(function test() {
+  if (localStorage.getItem("theme") === "theme-dark") {
+    setTheme("theme-dark");
+  } else {
+    setTheme("theme-light");
+  }
 })();
+
+//função para setar o tema no local storage do navegador
+
+//função para capturar o tema no local storage do navegador
