aeffb8a9de54749e942ff6b256d95a9b094bf153
guilherme amorim <cezarguilherme03@gmail.com>
2024-04-09 18:21:20 -0300

    operadores e variáveis

---
+++ JavaScript/operadoresLogicos.js
@@ -0,0 +1,32 @@
+const a = false, b = false
+
+//OR - ||
+//AND - &&
+
+//  F    F 
+if (a && b) {
+    console.log('VERDADEIRO');
+} else {
+    console.log('FALSE');
+}
+
+
+
+//EXEMPLO DE CADASTRO
+const email = 'guilherme@guilherme.com'
+const senha = 1234567
+
+//           TRUE                             FALSE
+if (email == 'guilherme@guilherme.com' || senha == 123456) {
+    console.log('Login aprovado! seja bem vindo(a)!');
+} else{
+    console.log('Usuário ou senha incorretos!');
+}
+
+//EXEMPLO FINAL
+const documento = false
+const vacinacao = true
+
+if (idade >= 18 ) {
+    
+}
--- JavaScript/variaveis.js
+++ JavaScript/variaveis.js
@@ -7,21 +7,20 @@ console.log(variavelLet);
 console.log(variavelVar);
 
 variavelLet = 10;
-variavelVar = "oi";
+variavelVar = "10 + 4";
 
 console.log(variavelConst);
 console.log(variavelLet);
 console.log(variavelVar);
 
-
 //Padrão de escrita: camelCase(variáveis), PascalCase(Funções)
 
 //declara variavel (let, var, const) - Identificador - Valor
-let anoCarro = 2010
-let modeloCarro = 'Sandero'
-let marcaCarro = 'Renault'
-let corCarro = 'Vermelho'
-let chassiCarro = 12345678
+let anoCarro = 2010;
+let modeloCarro = "Sandero";
+let marcaCarro = "Renault";
+let corCarro = "Vermelho";
+let chassiCarro = 12345678;
 
 //Métodos utilizados para juntar variável com texto: Interpolação ou Concatenação
 
@@ -29,4 +28,6 @@ let chassiCarro = 12345678
 // console.log("Ano do carro: " + anoCarro + " Modelo do carro: " + modeloCarro);
 
 //interpolação
-console.log(`Ano do carro: ${anoCarro}, Modelo do carro: ${modeloCarro}, marca do carro: ${marcaCarro}, cor do carro: ${corCarro}, chassi: ${chassiCarro}` );
+console.log(
+  `Ano do carro: ${anoCarro}, Modelo do carro: ${modeloCarro}, marca do carro: ${marcaCarro}, cor do carro: ${corCarro}, chassi: ${chassiCarro}`
+);
